577cb0dd9a6cc9a079c343b611edf1bea8d37571
ceotruelemon <ceotruelemon@gmail.com>
2026-01-01 19:29:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-function openForm(role) {
-  document.getElementById("contact").scrollIntoView({
+function scrollToForm() {
+  document.getElementById("form").scrollIntoView({
     behavior: "smooth"
   });
-
-  setTimeout(() => {
-    document.getElementById("roleSelect").value = role;
-    toggleRoleFields();
-  }, 400);
 }
-
-function toggleRoleFields() {
-  const role = document.getElementById("roleSelect").value;
-  document.getElementById("freelancerFields").style.display =
-    role === "Freelancer" ? "block" : "none";
-  document.getElementById("clientFields").style.display =
-    role === "Client" ? "block" : "none";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  document
-    .getElementById("roleSelect")
-    .addEventListener("change", toggleRoleFields);
-});
